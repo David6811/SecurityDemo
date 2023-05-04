2afa0b82acb99445229e03ce97bcc6cc538b1cb9
weixu <xuwei19850423@gmail.com>
2023-05-04 18:32:30 +1000

    config App.java

--- Gateway/src/main/java/org/helloai/gateway/App.java
+++ Gateway/src/main/java/org/helloai/gateway/App.java
@@ -1,13 +1,17 @@
 package org.helloai.gateway;
 
-/**
- * Hello world!
- *
- */
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication()
+@EnableEurekaClient
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        SpringApplication.run(App.class, args);
     }
 }
