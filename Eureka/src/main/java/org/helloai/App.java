5580504bdbb651a28e61ca2d76db41e0278363e7
weixu <xuwei19850423@gmail.com>
2023-05-04 18:01:57 +1000

    config App.java

--- Eureka/src/main/java/org/helloai/App.java
+++ Eureka/src/main/java/org/helloai/App.java
@@ -1,13 +1,15 @@
 package org.helloai;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class App {
+
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
     }
 }
