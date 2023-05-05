7200b9ff84902338925ab674519daf573742c5ab
weixu <xuwei19850423@gmail.com>
2023-05-05 15:36:35 +1000

    add schema.sql and data.sql

---
+++ Gateway/src/main/resources/sql/schema.sql
@@ -0,0 +1,12 @@
+drop schema if exists `securityDemo`;
+create schema `securityDemo`;
+use `securityDemo`;
+
+create table user(
+  userid varchar(32) NOT NULL primary key,
+  email varchar(255) default NULL COMMENT 'email',
+  password varchar(255) default NULL COMMENT 'password');
+
+
+
+
