f81bb892e663784ee9524d19235bce8175c65513
weixu <xuwei19850423@gmail.com>
2023-05-06 17:25:24 +1000

    config basic SecurityWebFilterChain

---
+++ Gateway/src/main/java/org/helloai/gateway/SecurityConfiguration.java
@@ -0,0 +1,22 @@
+package org.helloai.gateway;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableReactiveMethodSecurity;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@Configuration
+@EnableWebFluxSecurity
+@EnableReactiveMethodSecurity
+public class SecurityConfiguration {
+
+  @Bean
+  SecurityWebFilterChain webFluxSecurityFilterChain(ServerHttpSecurity http) {
+    http.csrf(csrf -> csrf.disable())
+        .authorizeExchange()
+        .anyExchange().permitAll();
+    return http.build();
+  }
+}
