8cc86d36155e1b8c99d0d245ec9524017d5b7a16
weixu <xuwei19850423@gmail.com>
2023-05-09 14:26:28 +1000

    no need jwtTokenUtil in SecurityConfiguration

--- Gateway/src/main/java/org/helloai/gateway/SecurityConfiguration.java
+++ Gateway/src/main/java/org/helloai/gateway/SecurityConfiguration.java
@@ -2,22 +2,15 @@ package org.helloai.gateway;
 
 import org.helloai.gateway.handler.AuthenticationFaillHandler;
 import org.helloai.gateway.handler.AuthenticationSuccessHandler;
-import org.helloai.gateway.jwt.JwtTokenUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.security.config.annotation.method.configuration.EnableReactiveMethodSecurity;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.server.SecurityWebFilterChain;
-import org.springframework.util.StringUtils;
-import org.springframework.web.server.ServerWebExchange;
-import org.springframework.web.server.WebFilter;
-import org.springframework.web.server.WebFilterChain;
 
 @Configuration
 @EnableWebFluxSecurity
